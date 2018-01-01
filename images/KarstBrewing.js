4ea8239c884eedc68b01304613450dc2cc89c9b0
Adam Luoma <aluoma26@gmail.com>
2018-01-01 13:32:41 -0600

    initial commit

---
+++ images/KarstBrewing.js
@@ -0,0 +1,28 @@
+var youlikeMeat = 5;
+
+if (youlikeMeat > 10 || youlikeMeat < 20){
+
+	console.log("Have a BRUGERERR");
+
+} else{
+
+	console.log("FUCK YURSELF"); 
+}
+
+var age = 22;
+
+while (age < 25) {
+
+	console.log("Your age is less than 25");
+	age++;
+
+}
+
+	console.log("Time to get Married");
+
+for (age2 = 26; age2 < 30; age2++) {
+
+	console.log("Getting older");
+}
+
+	console.log("31st Birthday");
